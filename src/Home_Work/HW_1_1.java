7d5d1f67e1dcfd1456f5e98390adef7c474c2be1
Kuba Bekoff <k.bekoff.5@gmail.com>
2024-02-25 22:28:13 +0600

    All home work #1

---
+++ src/Home_Work/HW_1_1.java
@@ -0,0 +1,14 @@
+package Home_Work;
+
+public class HW_1_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(3);
+        System.out.println(2);
+        System.out.println(1);
+        System.out.println("Поехали! ");
+
+        System.out.println("3 \n2 \n1 \nПоехали!");
+    }
+}
